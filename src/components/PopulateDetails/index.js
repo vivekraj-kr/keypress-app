ac996ab576861d54d4e72bbae14afac73831cb36
vivekraj_kr <vivekrajkr.mail@gmail.com>
2021-11-28 16:43:30 +0530

    Cleanup

--- src/components/PopulateDetails/index.js
+++ src/components/PopulateDetails/index.js
@@ -2,7 +2,6 @@ import React, { useContext } from 'react';
 import { DetailsContext } from '../../App';
 
 const PopulateDetails = ({ className }) => {
-  console.log();
   const shortCutsInfo = useContext(DetailsContext);
   return (<div className={className}> {shortCutsInfo.map(({ combo, description }, index) => {
     return <div key={index}>
